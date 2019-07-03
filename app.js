805418a1d75b5973cb4864049f52cd5a262129ef
Fabio <fabiofonseca@Fabios-MacBook-Pro.local>
2019-07-03 10:45:20 -0500

    modifing the file for create new capitalize name

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  if(req.query.nombre){
-    res.send(`<h1>Hola ${req.query.nombre}!</h1>`)
-  } else {
-    res.send(`<h1>Hola desconocido!</h1>`);
-  }
+app.get('/maker/:nombre', (req, res) => {
+    let name = req.params.nombre;
+    name = name[0].toUpperCase() + name.slice(1);
+    res.send(`<h1>Hola ${name}!</h1>`);
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
